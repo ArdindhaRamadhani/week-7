10fbf0ba07f4f60c8cbba4f13ce67d3cbc04ba1b
ArdindhaRamadhani <ardindharrmdhani@gmail.com>
2026-05-13 10:10:50 +0700

    elektronik dan makanan

--- Elektronik.java
+++ Elektronik.java
@@ -1,3 +1,21 @@
-public class Elektronik {
+public class Elektronik extends Produk {
 
+    private int masaGaransi;
+
+    public Elektronik(String nama, double harga, int stok, int masaGaransi) {
+        super(nama, harga, stok);
+        this.masaGaransi = masaGaransi;
+    }
+
+    @Override
+    public double hitungHargaAkhir(int jmlBeli) {
+        System.out.println("Total Harga= " + hitungTotalHarga(jmlBeli));
+        return hitungTotalHarga(jmlBeli) * 0.85;
+    }
+
+    @Override
+    public void tampilInfo() {
+        super.tampilInfo();
+        System.out.println("Masa Garansi: " + masaGaransi + "Tahun");
+    }
 }
--- Makanan.java
+++ Makanan.java
@@ -1,3 +1,20 @@
-public class Makanan {
+public class Makanan extends Produk {
+    private int kadaluarsa;
 
+    public Makanan(String nama, double harga, int stok, int kadaluarsa) {
+        super(nama, harga, stok);
+        this.kadaluarsa = kadaluarsa;
+    }
+
+    @Override
+    public double hitungHargaAkhir(int jmlBeli) {
+        System.out.println("Total Harga= " + hitungTotalHarga(jmlBeli));
+        return hitungTotalHarga(jmlBeli) * 0.95;
+    }
+
+    @Override
+    public void tampilInfo() {
+        super.tampilInfo();
+        System.out.println("Masa Kaduluarsa: " + kadaluarsa + " Hari");
+    }
 }
