fc73f4225c16dd8f632544789ed19e36cf6c2750
ArdindhaRamadhani <ardindharrmdhani@gmail.com>
2026-05-13 10:03:05 +0700

    Produk p1 dan p2

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class main {
     public static void main(String[] args) {
+        Produk p1 = new Elektronik("Samsung", 100000, 5, 2);
+        Produk p2 = new Makanan("Bakso", 1000, 100, 3);
 
     }
 }
